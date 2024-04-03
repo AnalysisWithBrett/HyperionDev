10dba6ddd09fd4f64e7f11f3a1aab9383ac40b12
Brett <148084194+AnalysisWithBrett@users.noreply.github.com>
2024-04-03 16:30:41 +0100

    Create helloWorld.py

---
+++ Getting started/helloWorld.py
@@ -0,0 +1,22 @@
+# Print statement
+print("Hello World")
+print(23)
+
+# Integer
+num_one = 100
+
+# Float
+float_one = 12.34
+
+print(num_one + float_one)
+
+# String
+my_name = "Rusty"
+print(my_name)
+
+# Boolean
+is_poke_out = True
+is_poke_hungry = False
+
+# Nonetype
+default_value = None
